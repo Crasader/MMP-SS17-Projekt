27ea9b1327f8f02ce67fc4b3692777c62fea37fb
Klara Maria Dersche <KlaraMariaDersche@dhcp18.cip.ifi.lmu.de>
2017-07-07 15:34:38 +0200

    asldjkflöksd

--- Classes/LevelHardScene.h
+++ Classes/LevelHardScene.h
@@ -13,7 +13,7 @@ public:
 	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
 	virtual bool init();
 
-	// implement the "static create()" method manually
+	// implement the "static create()" method manually asdjföasdjfö
 	CREATE_FUNC(LevelHardScene);
 
 private:
